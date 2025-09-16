136dd1eb145738f9feebdc3fb06d2bf3344681ad
Aleyna Yıldız <aleynayildizc@gmail.com>
2025-09-16 11:54:19 +0300

    Güncellemeler eklendi, geçici dosyalar atlandı

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE girisYap
+    @kulAdi varchar(50),
+    @sifre varchar(50)
+AS
+BEGIN
+    SELECT * FROM tbl_Login where kulAdi = @kulAdi and sifre = @sifre
+    INSERT INTO tbl_Login (kulAdi, sifre)
+    VALUES (@kulAdi, @sifre);
+END
